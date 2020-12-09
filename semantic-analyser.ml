3fe97fd434cfe62067453804b45af45f0527c23a
netanelpr <pratn@post.bgu.ac.il>
2020-12-09 18:49:19 +0200

    Implement the tail call anotation

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -61,6 +61,19 @@ end;;
 
 module Semantics : SEMANTICS = struct
 
+let make_set_expr' = fun var expr' ->
+  Set' (var, expr');;
+let make_def_expr' = fun var expr' ->
+  Def' (var, expr');;
+let make_seq_expr' = fun expr'_list ->
+  Seq' expr'_list;;
+let make_or_expr' = fun expr'_list ->
+  Or' expr'_list;;
+let make_lambda_simple_expr' = fun arg_names body_expr' ->
+  LambdaSimple' (arg_names, body_expr');;
+let make_lambda_opt_expr' = fun arg_names opt_arg_name body_expr' ->
+  LambdaOpt' (arg_names, opt_arg_name, body_expr');;
+
 let annotate_lexical_addresses_helper = fun e ->
   let vars_map = Hashtbl.create 16 in
   let rec annotate_set_bang = fun depth var expr ->
@@ -148,6 +161,66 @@ let annotate_lexical_addresses_helper = fun e ->
     | Applic (expr_operator, exprs_operands) -> annotate_applic depth expr_operator exprs_operands in
 
   ann_lex_addr_traversal 0 e;;
+
+let annotate_tail_calls_helpr = fun e ->
+
+  let rec annotate_if = fun test dit dif is_in_tp ->
+    let annotate_test = annotate_traversal test false in
+    let annotate_dit = annotate_traversal dit is_in_tp in
+    let annotate_dif = annotate_traversal dif is_in_tp in
+      If'(annotate_test, annotate_dit, annotate_dif)
+
+  and annotate_list_not_in_tail_pos = fun expr'_list ->
+    List.map (fun expr' -> annotate_traversal expr' false) expr'_list
+
+  and annotate_list = fun expr'_list is_in_tp factory ->
+    let rev_list = List.rev expr'_list in 
+    match rev_list with
+    | [] -> factory []
+    | expr' :: rest -> 
+        let annotate_rev_list = annotate_list_not_in_tail_pos rev_list in
+        let annotate_last = annotate_traversal expr' is_in_tp in
+        let annotate_list = List.rev (annotate_last :: annotate_rev_list) in
+          factory annotate_list
+
+  and annotate_assignment_expr' = fun var expr' factory ->
+    factory var (annotate_traversal expr' false)
+
+  and annotate_lambda = fun body_expr' factory ->
+    factory (annotate_traversal body_expr' true)
+
+  and annotate_lambda_simple = fun args_names body_expr' ->
+    annotate_lambda body_expr' (fun annotated_body_expr' -> make_lambda_simple_expr' args_names annotated_body_expr')
+  
+  and annotate_lambda_opt = fun args_names arg_name_opt body_expr' ->
+    annotate_lambda body_expr' (fun annotated_body_expr' -> make_lambda_opt_expr' args_names arg_name_opt annotated_body_expr')
+
+  and annotate_applic = fun expr'_operator exprs'_operands is_in_tp ->
+    let annotate_exprs'_operands = annotate_list_not_in_tail_pos exprs'_operands in
+    let annotate_expr'_operator = annotate_traversal expr'_operator is_in_tp in
+      if is_in_tp
+      then ApplicTP'(annotate_expr'_operator, annotate_exprs'_operands)
+      else Applic'(annotate_expr'_operator, annotate_exprs'_operands)
+
+  and annotate_traversal = fun expr' is_in_tp ->
+  match expr' with
+  | Const' sexpr -> e
+  | Var' var -> e
+  | Box' var -> e
+  | BoxGet' var -> e
+  | BoxSet' (var, expr') -> e
+  | If' (test, dit, dif) -> annotate_if test dit dif is_in_tp
+  | Seq' expr'_list -> annotate_list expr'_list is_in_tp make_seq_expr'
+  | Set' (var, expr') -> annotate_assignment_expr' var expr' make_set_expr'
+  | Def' (var, expr') -> annotate_assignment_expr' var expr' make_def_expr'
+  | Or' expr'_list -> annotate_list expr'_list is_in_tp make_or_expr'
+  | LambdaSimple' (args_names, body_expr') -> annotate_lambda_simple args_names body_expr'
+  | LambdaOpt' (args_names, arg_name_opt, body_expr') -> annotate_lambda_opt args_names arg_name_opt body_expr'
+  | Applic' (expr'_operator, exprs'_operands) -> annotate_applic expr'_operator exprs'_operands is_in_tp
+  | ApplicTP' (expr'_operator, exprs'_operands) -> raise X_syntax_error in
+  
+  annotate_traversal e false;;
+
 
 let annotate_lexical_addresses e = annotate_lexical_addresses_helper e;;
 
