f8f74c13af6dae198fa93e75ad40a93d9005bc25
netanelpr <pratn@post.bgu.ac.il>
2020-12-31 21:37:13 +0200

    Implement the creation of const table tuple list
    Implement it for one expr'

--- code-gen.ml
+++ code-gen.ml
@@ -99,7 +99,7 @@ module Code_Gen (* : CODE_GEN *) = struct
   | ApplicTP'(operator_expr', operands_expr'_list) ->
       extract_const_from_applic operator_expr' operands_expr'_list;;
 
-  (* Step 3 and 4 extend the const list and remove duplicates *)
+  (* Step 3  extend the const list *)
   let rec extend_const = fun const ->
     match const with
     | Void -> [Void]
@@ -215,53 +215,79 @@ module Code_Gen (* : CODE_GEN *) = struct
 
   (* step 5 *)
   let build_consts_tbl = fun const_list ->
-    let rec build_consts_tbl_rec = fun sexpr ->
-      raise X_not_yet_implemented
-    and make_row_for_const = fun const current_const_tbl index ->
-      raise X_not_yet_implemented
+    let rec build_consts_tbl_rec = fun const_list tuple_const_list index ->
+      match const_list with
+      | [] -> tuple_const_list
+      | const :: rest -> (
+        let const_asm_code = make_asm_code const tuple_const_list index in
+        let const_sob_size =  get_sob_size_of_const const in
+        let const_index = index + const_sob_size in
+        let const_tuple = (const, (index, const_asm_code)) in
+         build_consts_tbl_rec rest (tuple_const_list @ [const_tuple]) const_index
+      )
+
+    and get_const_from_tuple = fun const_tuple ->
+        match const_tuple with
+        | (const, (_, _)) -> const
+
     and get_index_from_tuple = fun const_tuple ->
         match const_tuple with
         | (_, (index, _)) -> index
-    and make_asm_code =  fun const current_const_tbl index ->
+
+    and get_tuple_of_const_from_tbl = fun const const_tbl ->
+      let eq = constant_eq const in
+        List.find 
+        (fun tuple -> 
+          let c = get_const_from_tuple tuple in
+            eq c
+        )
+        const_tbl
+
+    and make_asm_code_number_from_const_tbl = fun number index ->
+      match number with
+      | Fraction(num, den) -> make_literal_rational_asm_code num den
+      | Float(float) -> make_literal_float_asm_code float
+
+    and make_asm_code_symbol_from_const_tbl = fun symbol_string current_tuple_const_tbl ->
+      let string_const = Sexpr(String(symbol_string)) in
+      let sting_tuple = get_tuple_of_const_from_tbl string_const current_tuple_const_tbl in
+      let string_index = get_index_from_tuple sting_tuple in
+        make_literal_symbol_asm_code string_index
+
+    and make_asm_code_pair_from_const_tbl = fun car cdr current_tuple_const_tbl ->
+      let car_tuple = get_tuple_of_const_from_tbl (Sexpr(car)) current_tuple_const_tbl in
+      let cdr_tuple = get_tuple_of_const_from_tbl (Sexpr(cdr)) current_tuple_const_tbl in
+      let car_index = get_index_from_tuple car_tuple in
+      let cdr_index = get_index_from_tuple cdr_tuple in
+        make_literal_pair_asm_code car_index cdr_index
+
+    and make_asm_code =  fun const current_tuple_const_tbl index ->
       match const with
       | Void -> make_literal_void_asm_code
       | Sexpr(sexpr) -> (
         match sexpr with
         | Nil -> make_literal_nil_asm_code
         | Bool(bool) -> make_literal_bool_asm_code bool
-        | Number(number) -> (
-            match number with
-            | Fraction(num, den) -> make_literal_rational_asm_code num den
-            | Float(float) -> make_literal_float_asm_code float
-        )
+        | Number(number) -> 
+            make_asm_code_number_from_const_tbl number index
         | Char(ch) -> make_literal_char_asm_code ch
-        | String(string) -> make_literal_string_asm_code string
-        | Symbol(symbol_string) -> (
-          let sting_tuple = 
-            List.find
-            (fun (const, _) -> 
-              match const with
-              | Sexpr(String(string)) -> String.equal symbol_string string
-              | _ -> false
-            )             
-            current_const_tbl in
-          let string_tuple_index = get_index_from_tuple sting_tuple in
-            make_literal_symbol_asm_code string_tuple_index 
-        )
-        | Pair(car, cdr) -> (
-            (* after the sorting, the car is in the last index - 1 of the tuple 
-               and the cdr is in the last index
-            *)
-            let const_list_length = List.length current_const_tbl in
-            let car_tuple = List.nth current_const_tbl (const_list_length-2) in
-            let cdr_tuple = List.nth current_const_tbl (const_list_length-1) in
-            let car_index = get_index_from_tuple car_tuple in
-            let cdr_index = get_index_from_tuple cdr_tuple in
-              make_literal_pair_asm_code car_index cdr_index
-        )) in
-        (* edit *)
-        build_consts_tbl_rec;;
+        | String(string) -> 
+            make_literal_string_asm_code string
+        | Symbol(string_symbol) -> 
+            make_asm_code_symbol_from_const_tbl string_symbol current_tuple_const_tbl
+        | Pair(car, cdr) -> 
+            make_asm_code_pair_from_const_tbl car cdr current_tuple_const_tbl
+      ) in
+    build_consts_tbl_rec const_list [] 0;;
 
+  let build_const_tbl_tuple_list = fun expr' ->
+    let extracted_consts_list = extract_all_const expr' in
+    let extracted_consts_list = 
+      remove_dup_consts_from_const_list extracted_consts_list in
+    let extended_consts_list = extend_const_list (extracted_consts_list) in
+    let extracted_consts_list = 
+      remove_dup_consts_from_const_list extended_consts_list in
+    build_consts_tbl extracted_consts_list;;
 
   let make_consts_tbl asts = raise X_not_yet_implemented;;
   let make_fvars_tbl asts = raise X_not_yet_implemented;;
