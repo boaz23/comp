104b883b9de02cf21f4d1bf6e051d4232ac00ecb
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-01-08 00:11:56 +0200

    Apply test cases

--- tests_code_gen/test_apply_inc.scm
+++ tests_code_gen/test_apply_inc.scm
@@ -1,1 +1,17 @@
 (apply (lambda () 'ok) '())
+(apply (lambda (x y z) (cons y (cons x z))) 1 "a" '(ok))
+
+(define funny
+  (lambda (n)
+    (if (zero? n)
+        '()
+        (apply cons `(ha ,(funny (- n 1)))))))
+
+(define funnier
+  (lambda (n)
+    (if (zero? n)
+        '()
+        `(ha ,@(apply cons `(ha ,(funny (- n 1))))))))
+
+(funny 5)
+(funnier 5)
