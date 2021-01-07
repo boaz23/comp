b26cfb5913834556ec77aea1a88a94535912662a
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-01-07 14:55:33 +0200

    ApplicTP code example changes

--- tests_code_gen/test_applictp_inc.scm
+++ tests_code_gen/test_applictp_inc.scm
@@ -1,5 +1,6 @@
-(define f (lambda (x) (g x)))
-(define g (lambda (x) (h (cons x '()))))
+(define z '())
+(define f (lambda (x) (set! z x) (g)))
+(define g (lambda () (h (cons z '()))))
 (define h (lambda (x) (cons 1 x)))
 
 (define p (lambda (x) (cons "hello" x)))
