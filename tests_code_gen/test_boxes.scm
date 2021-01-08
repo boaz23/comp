4974a5344adcfab88e3c28662c3de2a9efa658fe
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-01-08 14:30:50 +0200

    Compiler test cases

--- tests_code_gen/test_boxes.scm
+++ tests_code_gen/test_boxes.scm
@@ -7,15 +7,15 @@
             n)
           (cons (lambda ()
             (set! n 0)) '()))))))
-(define x (f 5))
-x
-(set! x (x))
-x
-(car x)
-((car (cdr x)))
-(car x)
-((car (cdr (cdr x))))
-(car x)
+(define y (f 5))
+y
+(set! y (y))
+y
+(car y)
+((car (cdr y)))
+(car y)
+((car (cdr (cdr y))))
+(car y)
 
 (define g (lambda (n)
     (cons
@@ -26,3 +26,22 @@ x
       (cons (lambda ()
         (set! n 0)) '())))))
 (g 5)
+
+(define h (lambda (n)
+      (lambda ()
+        (cons
+          (lambda () n)
+          (cons (lambda ()
+            (set! n (+ n 1))
+            n)
+          (cons (lambda ()
+            (set! n 0)) '()))))))
+(define x (h 5))
+x
+(set! x (x))
+x
+((car x))
+((car (cdr x)))
+((car x))
+((car (cdr (cdr x))))
+((car x))
