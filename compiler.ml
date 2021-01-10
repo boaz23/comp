b6c1212169da26666076ac29ce21d7995ed3d0e7
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-01-10 18:33:21 +0200

    Debug flag set from make arguments
    The debug flag is now set from the make arguments.
    The command-line arguments is "-d"

--- compiler.ml
+++ compiler.ml
@@ -1,6 +1,6 @@
+#use "config.ml";;
 #use "code-gen.ml";;
 #use "prims.ml";;
-#use "config.ml";;
 
 (*
    Auxiliary function to load the contents of a file into a string in memory.
--- config.ml
+++ config.ml
@@ -1,10 +1,18 @@
 
 module type COMP_CONFIG = sig
-  val debug: bool
+  val debug : bool
 end;;
 
 module CompConfig : COMP_CONFIG = struct
 
-let debug = true;;
+(* set the debug flag if present
+   so that they will see the flag *)
+(* it has to be defined here because each use seemse
+   to import a new module instance each time and thus
+   other modules do not see the change in the flag *)
+let debug = Array.mem "-d" Sys.argv;;
+if debug then begin
+  Printf.printf "; debug flag is on\n";
+end;
 
 end;;
